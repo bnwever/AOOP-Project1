9318f4df558d6e8b61ac404417160b7d5c4a7944
juztchuy <92234789+juztchuy@users.noreply.github.com>
2024-10-16 17:21:02 -0600

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,3 +1,17 @@
 public class Person {
+    private String name;
+    private String address;
+
+    public Person(String nameIn , String addressIn ){
+        this.name = nameIn;
+        this.address = addressIn;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getAddress(){
+        return this.address;
+    }
+    
     
 }
