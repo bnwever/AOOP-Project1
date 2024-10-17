88de10914be5c4c802a7df566a8c39a43f7b3aaf
juztchuy <92234789+juztchuy@users.noreply.github.com>
2024-10-16 19:29:06 -0600

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,17 +1,40 @@
+/**
+ * The Person class represents a person with a name and an address.
+ * This class provides basic information about a person.
+ * 
+ * @author Jesus Ordaz
+ */
 public class Person {
     private String name;
     private String address;
 
-    public Person(String nameIn , String addressIn ){
+    /**
+     * Constructs a Person with the specified name and address.
+     * 
+     * @param nameIn the name of the person
+     * @param addressIn the address of the person
+     */
+    public Person(String nameIn, String addressIn) {
         this.name = nameIn;
         this.address = addressIn;
     }
-    public String getName(){
+
+    /**
+     * Gets the name of the person.
+     * 
+     * @return the name of the person
+     */
+    public String getName() {
         return this.name;
     }
-    public String getAddress(){
+
+    /**
+     * Gets the address of the person.
+     * 
+     * @return the address of the person
+     */
+    public String getAddress() {
         return this.address;
     }
-    
-    
 }
+
