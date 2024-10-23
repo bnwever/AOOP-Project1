355eb8964fcf2323c96ed580e8265e91a9d0653a
juztchuy <92234789+juztchuy@users.noreply.github.com>
2024-10-23 08:54:30 -0600

    Update Transaction_Log.java

--- Transaction_Log.java
+++ Transaction_Log.java
@@ -1,33 +1,57 @@
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * The TransactionLog class is for logging and retrieving
- * transaction details from the bank
+ * transaction details from the bank using a fixed-size array.
  * 
  * <p>Provides methods to log transactions and read the transaction log.
  * 
  * @author Jesus Ordaz
  */
-public class Transaction_Log {
+public class TransactionLog {
 
-    private List<String> logEntries = new ArrayList<>();
+    private String[] logEntries;
+    private int currentIndex;
+
+    // Constructor
+    /**
+     * Constructs a TransactionLog with a specified capacity.
+     * 
+     * @param capacity The maximum number of transactions that can be logged.
+     */
+    public TransactionLog(int capacity) {
+        this.logEntries = new String[capacity];
+        this.currentIndex = 0;
+    }
 
     /**
      * Logs a transaction by adding the given details to the transaction log.
+     * If the log is full, the oldest entry will be overwritten.
      *
      * @param details A string containing the details of the transaction.
      */
     public void logTransaction(String details) {
-        logEntries.add(details);
+        if (currentIndex >= logEntries.length) {
+            // Overwrite the oldest transaction when the log is full
+            currentIndex = 0;
+        }
+        logEntries[currentIndex] = details;
+        currentIndex++;
     }
 
     /**
-     * Reads the transaction log and returns a list of all logged transactions.
-     *
-     * @return A list of strings, where each string represents a logged transaction.
+     * Reads the transaction log and returns an array of all logged transactions.
+     * 
+     * @return An array of strings, where each string represents a logged transaction.
      */
-    public List<String> readLog() {
-        return new ArrayList<>(logEntries);
+    public String[] readLog() {
+        return logEntries.clone();
+    }
+
+    /**
+     * Returns the total capacity of the transaction log.
+     * 
+     * @return The capacity of the transaction log.
+     */
+    public int getCapacity() {
+        return logEntries.length;
     }
 }
